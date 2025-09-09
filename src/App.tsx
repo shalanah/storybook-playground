bd9cfcc25da8ea9381ec3cdced1f71e0fc19f185
shalanah <shalanahfaith@gmail.com>
2025-09-09 12:43:42 -0500

    svgr demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import { Layout } from "./components/Layout";
+import PenIcon from "./icons/pen.svg?react";
+import PiggyIcon from "./icons/piggy.svg?react";
+import SunIcon from "./icons/sun.svg?react";
+import DemoIcon from "./icons/demo.svg?react";
 
 function App() {
-  return <Layout>Hey</Layout>;
+  return (
+    <Layout>
+      <section>
+        <h1 className="flex gap-2 text-4xl">
+          Content
+          <DemoIcon title={"Demo"} />
+          <PiggyIcon title={"Piggy Bank"} />
+          <SunIcon title={"Sun"} />
+          <PenIcon
+            // className="[&_*]:[vector-effect:non-scaling-stroke]"
+            // style={{ color: "purple" }}
+            // className="text-red-500"
+            // width={200}
+            // height={"auto"}
+            title={"Pen"}
+          />
+        </h1>
+      </section>
+    </Layout>
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
+// @ts-nocheck - just for now while showing off svgr
+
 /// <reference types="vitest/config" />
-import { defineConfig } from "vite";
+import { defineConfig, type PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 import path from "node:path";
@@ -14,7 +17,30 @@ const dirname =
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr({
+      svgrOptions: {
+        plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx"],
+        icon: true,
+        svgoConfig: {
+          plugins: [
+            {
+              name: "preset-default",
+              params: {
+                overrides: {
+                  removeViewBox: false, // keep viewBox
+                },
+              },
+            },
+            { name: "removeAttrs", params: { attrs: "(class|data-name)" } },
+          ],
+        },
+      },
+      include: "**/*.svg?react",
+    }),
+  ] satisfies PluginOption[],
   test: {
     projects: [
       {
